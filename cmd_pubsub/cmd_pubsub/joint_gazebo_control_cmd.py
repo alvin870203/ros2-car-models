1c4c419c8b366a34dd7282c178c96d8ba793acdd
alvin870203 <alvin870203@gmail.com>
2023-11-04 15:17:15 +0000

    Change control cmd node from c++ into python

---
+++ cmd_pubsub/cmd_pubsub/joint_gazebo_control_cmd.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(JointTrajectory, 'autocar/set_joint_trajectory', 10)
+        timer_period = 0.01  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        joints_cmd = JointTrajectory()
+
+        joints_cmd.header.frame_id = "base_link"
+        joints_cmd.joint_names = ["l_steer",
+                                  "r_steer",
+                                  "fl_axle",
+                                  "fr_axle",
+                                  "bl_axle",
+                                  "br_axle"]
+        joints_cmd.points.append(JointTrajectoryPoint(positions=[-0.5,
+                                                                 -0.5,
+                                                                 0.0,
+                                                                 0.0,
+                                                                 0.0,
+                                                                 0.0]))
+
+        self.publisher_.publish(joints_cmd)
+        self.get_logger().info('Publishing: joints_cmd')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
