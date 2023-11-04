f709098711564ea80ee6f4817410d52c60bb1100
alvin870203 <alvin870203@gmail.com>
2023-11-04 15:39:07 +0000

    Remove unnecessary line

--- cmd_pubsub/cmd_pubsub/joint_gazebo_control_cmd.py
+++ cmd_pubsub/cmd_pubsub/joint_gazebo_control_cmd.py
@@ -11,7 +11,6 @@ class MinimalPublisher(Node):
         self.publisher_ = self.create_publisher(JointTrajectory, 'autocar/set_joint_trajectory', 10)
         timer_period = 0.01  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
 
     def timer_callback(self):
         joints_cmd = JointTrajectory()
